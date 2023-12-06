a5f87d47ec5cd597b04b07f9c57ceb735b92f5fc
MarisaS1978 <msandroni@gmail.com>
2023-12-06 12:26:21 -0300

    Series se muetran como cards

--- FrontEndCatalogo/js/miLista.js
+++ FrontEndCatalogo/js/miLista.js
@@ -11,17 +11,22 @@ createApp({
     },
     methods: {
         fetchData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    this.series = data;
-                    this.cargando = false;
-                })
-                .catch(err => {
-                    console.error(err);
-                    this.error = true;
-                });
-        },
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                this.series = data;
+                this.cargando = false;
+            })
+            .catch(err => {
+                console.error('Error fetching data:', err);
+                this.error = true;
+            });
+    },
     },
     created() {
         this.fetchData(this.url);
